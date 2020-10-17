6814f33d7f82e92b08f059001d20e2ac540029d7
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-10-17 20:36:03 +0900

    stdout 버퍼 삭제(즉시 출력)

--- src/silo.cpp
+++ src/silo.cpp
@@ -29,6 +29,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 
 int main(int argc, char **argv) {
+    setbuf(stdout, nullptr);
+
     NODE *n, *m;
     NODE *node;
     WIRE *x, *y, *z, *w;
